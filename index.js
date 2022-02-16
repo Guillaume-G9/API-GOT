3970284f4fb88efaca25251afd785a906adfc70b
Guillaume-G9 <ggonthier@live.fr>
2022-02-16 11:47:50 +0100

    init

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-let characters = document.querySelector("#people")
+const characters = document.querySelector('#people')
+const searchBar = document.querySelector('#searchBar')
+let charArray = [];
 
 
 async function gotCharacters(){
     let rep = await fetch('https://anapioficeandfire.com/api/characters/');
     let response = await rep.json();
+    charArray = response
+    display(charArray)
 
-    response.map(names => {
-        characters.innerHTML += `
-        <li>${names.aliases}</li>`
-    })
-    // function displayCharacter(){
-    //     characters.textContent += ` <li>${response.aliases}</li>`;
-    // }
-    
 }
 
+gotCharacters();
 
-gotCharacters()
+function display(array) {
+    characters.innerHTML =""
+    array.map(names => {
+        characters.innerHTML += `
+        <li>${names.aliases}</li>`
+    }) 
+}
+
+searchBar.addEventListener('keyup', (event) => {
+    let input = event.target.value.toLowerCase();
+    const filter = charArray.filter(noms => {
+        return noms.aliases.toLowerCase().includes(input)
+    })
+    display(filter)
+})
+
+function playSound() {
+    document.querySelector("audio").loop = true;
+    document.querySelector("audio").play()
+}
+function stop() {
+
+
+    document.querySelector("audio").pause()
+}
